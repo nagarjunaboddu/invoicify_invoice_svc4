cc29aa975037065f2cd2a31d2e551b769e4adf73
Hemasree Darisiguntla <sreed2503@gmail.com>
2021-02-23 19:12:51 -0500

    Added service method and repository to save lineitem

--- src/main/java/com/invocify/invoice/entity/LineItem.java
+++ src/main/java/com/invocify/invoice/entity/LineItem.java
@@ -1,4 +1,25 @@
 package com.invocify.invoice.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LineItem {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String description;
+    private BigDecimal rate;
+    private String rateType;
+
 }
--- src/test/java/com/invocify/invoice/services/LineItemServiceTest.java
+++ src/test/java/com/invocify/invoice/services/LineItemServiceTest.java
@@ -1,18 +1,43 @@
 package com.invocify.invoice.services;
 
 import com.invocify.invoice.entity.LineItem;
+import com.invocify.invoice.repository.LineItemRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class LineItemServiceTest {
+
+    LineItemRepository lineItemRepository;
+    LineItemService lineItemService;
+
+    @BeforeEach
+    public void initialize() {
+        lineItemRepository = mock(LineItemRepository.class);
+        lineItemService = new LineItemService(lineItemRepository);
+    }
 
     @Test
     public void createLineItem() {
 
-        LineItemService lineItemService = new LineItemService();
-        LineItem lineItem = lineItemService.createLineItem();
-        assertEquals(null, lineItem);
+        LineItem expectedLineItem = LineItem.builder()
+                .description("tdd")
+                .rate(new BigDecimal(10))
+                .rateType("flat")
+                .id(UUID.randomUUID())
+                .build();
+
+        when(lineItemRepository.save(any(LineItem.class))).thenReturn(expectedLineItem);
+
+        LineItem actualLineItem = lineItemService.createLineItem(expectedLineItem);
+        assertEquals(expectedLineItem, actualLineItem);
+        verify(lineItemRepository, times(1)).save(any(LineItem.class));
 
     }
 }
